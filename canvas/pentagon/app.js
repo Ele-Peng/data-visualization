a38b257713fa2ce38da0424052550321c0286ed6
EllePeng <374237925@qq.com>
2020-07-21 11:15:04 +0800

    feat: 五角形-canvas

---
+++ canvas/pentagon/app.js
@@ -0,0 +1,51 @@
+import { Vector2D } from "../../common/lib/vector2d.js";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const {width, height} = canvas;
+ctx.translate(0.5 * width, 0.5 * height);
+ctx.scale(1, -1);
+
+// 构建五角形顶点
+const points = [new Vector2D(0, 100)];
+for (let i = 1; i <= 4; i ++) {
+  const tempPoint = points[0].copy().rotate(i * Math.PI * 0.4);
+  points.push(tempPoint);
+}
+const polygon = [...points];
+ctx.save();
+ctx.translate(-128, 128);
+draw(polygon, ctx, {fillStyle: "nonzero"}); // default is nonzero, you can try "evenodd"
+ctx.restore();
+
+
+const pentagon = [
+  points[0],
+  points[2],
+  points[4],
+  points[1],
+  points[3]
+]
+console.log(pentagon);
+ctx.save();
+ctx.translate(128, 128);
+draw(pentagon, ctx, {fillStyle: "nonzero"});
+ctx.restore();
+
+function draw(points, context, {strokeStyle = "black", fillStyle = null} = {}) {
+  console.log('fillStyle', fillStyle);
+  context.strokeStyle = strokeStyle;
+  context.beginPath();
+  context.moveTo(...points[0]);
+  for (let i = 1; i < points.length; i ++) {
+    console.log('i', i)
+    console.log('points[i]', points[i])
+    context.lineTo(...points[i]);
+  }
+  if (fillStyle) {
+    context.fillStyle = fillStyle;
+    context.fill();
+  }
+  context.closePath();
+  context.stroke();
+}
